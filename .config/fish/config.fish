d77526494d2f1362ba207e03c01cfd34b42bbe0a
Kevin Knosala <k.knosala@fz-juelich.de>
2024-02-24 00:43:02 +0100

    Add config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -g fish_greeting
+
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+~/.local/bin/mise activate fish | source
+
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+fzf_configure_bindings --directory=\ct 
+
